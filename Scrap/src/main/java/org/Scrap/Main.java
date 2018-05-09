10079329eb11ee32ea2a5a7c44049268fe0b0445
FelipeSantosSouza <felipesnt13@gmail.com>
2018-05-09 11:39:29 -0300

    atualizacao

--- Scrap/src/main/java/org/Scrap/Main.java
+++ Scrap/src/main/java/org/Scrap/Main.java
@@ -10,9 +10,13 @@ public class Main {
 		final Document documento = Jsoup.connect("https://www.imdb.com/chart/top").get();
 
 		for(Element row: documento.select("table.chart.full-width tr")) {
-			final String titulo = row.select(".titleColumn a").text();
-			final String classificacao = row.select(".imdbRating").text();
-
+			String titulo = null;
+			String classificacao = null;
+			if ((!row.equals(null) || (!row.equals(" ")))) {
+				titulo = row.select(".titleColumn a").text();
+				classificacao = row.select(".imdbRating").text();
+			}
+			
 			System.out.println("Titulo: "+ titulo + " Classificacao: "+ classificacao);
 			
 			
